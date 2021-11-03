2e8377562eafb08f36e490b53fd7ec3a768062d1
Alex <cnalex_2@hotmail.com>
2021-11-02 23:00:06 -0500

    se agrega contactos y logo

---
+++ contacto.js
@@ -0,0 +1,12 @@
+//-------------para formulario
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#trucazo')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    $buttonMailto.setAttribute('href', `mailto:cnalex_2@hotmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+    $buttonMailto.click()
+}
